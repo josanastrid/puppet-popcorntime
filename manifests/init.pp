e54a4b5e555ccd0e62ccd70658f749c722955e1f
Josan Dometita <josan.dometita@aestrea.com>
2014-08-28 00:00:55 +0800

    Parameterized popcorntime class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
 # Install Popcorn-Time from popcorntime.io
-class popcorntime {
+class popcorntime($version = '0.3.2') {
   package{ 'Popcorn-Time':
     provider => 'appdmg_eula',
-    source   => 'https://cdn.popcorntime.io/build/Popcorn-Time-0.3.2-Mac.dmg'
+    source   => "https://cdn.popcorntime.io/build/Popcorn-Time-${version}-Mac.dmg"
   }
 }
